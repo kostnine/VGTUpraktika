398d058402652d6ab43750fee8dff2aa7b31edd2
Laurynas <lauris.leisis@gmail.com>
2022-10-28 09:17:12 +0300

    Added Header and footer to page

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,26 @@
 <template>
-  
+  <div>
+    <h1>Footer</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
+    name: 'FooterComponent',
+    data(){
+        return{
 
+        }
+    }
 }
 </script>
 
-<style>
+<style scoped lang="scss">
+div{
 
+}
 </style>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,16 @@
 <template>
-  
+  <div>
+    <h1>Header</h1>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </div>
 </template>
 
 <script>
 export default {
+    name: 'HeaderComponent',
     data(){
         return{
 
@@ -12,6 +19,8 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style scoped lang="scss">
+div{
 
+}
 </style>
