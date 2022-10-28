6eb38c13514fffccd07a312076ba25b6bb0f0475
Laurynas <lauris.leisis@gmail.com>
2022-10-28 09:29:31 +0300

    added main variable scss file

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,8 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+@import './assets/scss/_variables.scss';
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
